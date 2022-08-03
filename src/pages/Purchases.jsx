ef7c0cff89f82854844e83d91620b9e05a285366
Jeanpol Giraldo <giraldojeanpool@hotmail.com>
2022-08-03 17:05:58 -0500

    'purchases'

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Card } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPurchasesThunk } from '../store/slices/purchases.slice';
 
@@ -13,18 +14,42 @@ const Purchases = () => {
 
     console.log(purchases)
 
+
+
     return (
         <div>
             <h1>Purchases</h1>
-            <ul>
+
+            <div>
                 {
                     purchases.map(purchase => (
-                        <li key={purchase.id}>
-                            {purchase.cart?.products?.title}
-                        </li>
+
+                        <Card key={purchase.id}>
+                            <Card.Header>{Date(purchase.createdAt).slice(3, 16)}</Card.Header>
+                            {
+                                purchase.cart.products.map(product => (
+
+                                    <div key={product.id} className="mg-10">
+
+                                        <div className='d-flex gap-3'>
+
+
+                                            <div><p>{product.title}</p></div>
+                                            <div><p>{product.productsInCart.quantity}</p></div>
+                                            <div><p>{product.price}</p></div>
+
+
+                                        </div>
+                                    </div>
+
+                                ))
+                            }
+                        </Card>
                     ))
                 }
-            </ul>
+
+
+            </div>
         </div>
     );
 };
--- src/utils/getConfig.jsx
+++ src/utils/getConfig.jsx
@@ -1,5 +1,5 @@
 const getConfig = () => ({
-    headers: { Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjE5LCJpYXQiOjE2NTkxODU1NjUsImV4cCI6MTY2NDM2OTU2NX0.djtQWScYEMl8ZP94vde0Msm7G7RGiL3fYkfgak2KDXs'}` }
+    headers: { Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU5MzgyMjExLCJleHAiOjE2NjQ1NjYyMTF9.AqEcbRcGUCNYrQHzCOWysno7hpwWIH1tVrJmD_cyd_U"}` }
 });
 
 export default getConfig;
