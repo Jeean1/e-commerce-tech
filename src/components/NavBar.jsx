28472cae54061f6ae525245c35efbdb640696247
Jeanpol Giraldo <giraldojeanpool@hotmail.com>
2022-08-04 16:39:56 -0500

    'rama-Jean'

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { DropDownMenu } from ".";
+
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" className="fixed-top">
       <Container>
-        <Navbar.Brand href="/#/" style={{color: '#f85555'}}>E-commerce</Navbar.Brand>
+        <Navbar.Brand href="/#/" style={{ color: '#f85555' }}>E-commerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/#/">Home</Nav.Link>
             <Nav.Link href="/#/login">Login</Nav.Link>
             <Nav.Link href="/#/purchases">Purchases</Nav.Link>
+
           </Nav>
+          <DropDownMenu />
         </Navbar.Collapse>
       </Container>
     </Navbar>
